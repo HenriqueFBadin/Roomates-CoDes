6927d32a5d29fc014ef62b99dda3b39b075a0bcc
HenriqueFBadin <henriquefb3@al.insper.edu.br>
2022-12-01 18:39:14 -0300

    report de Match para o usuário

--- js/script.js
+++ js/script.js
@@ -9,7 +9,8 @@ const person_1 = {
     },
     photos: img,
     likes: 0,
-    show: true
+    show: true,
+    match: 1
   };
 
 const person_2 = {
@@ -20,7 +21,8 @@ const person_2 = {
     },
     photos: img_2,
     likes: 0,
-    show: true
+    show: true,
+    match: 1
   };
 
 document.addEventListener('DOMContentLoaded', function(event){    
@@ -132,22 +134,21 @@ document.addEventListener('DOMContentLoaded', function(event){
     // Get the modal
     var modal = document.getElementById("myModal");
 
-    // Get the button that opens the modal
-    var btn = document.getElementById("myBtn");
-
     // Get the <span> element that closes the modal
     var span = document.getElementsByClassName("close")[0];
 
     // When the user clicks the button, open the modal 
-    btn.addEventListener('click', function(event) {
-        modal.style.display = "block";
-        console.log("oi")
+    likeness[0].addEventListener('click', function(event) {
+        if (dictionary[person_n].show == 1){
+            modal.style.display = "block";
+            console.log("oi")
+        }
     });
 
     // When the user clicks on <span> (x), close the modal
-    //span.addEventListener('click', function(event) {
-    //    modal.style.display = "none";
-    //});
+    span.addEventListener('click', function(event) {
+        modal.style.display = "none";
+    });
 
     // When the user clicks anywhere outside of the modal, close it
     window.addEventListener('click', function(event) {
