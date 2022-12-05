e8b1780b92365698f8710ce113a0ddaa25cbe354
HenriqueRBomfim <henriquerb1@al.insper.edu.br>
2022-12-05 20:00:07 -0300

    Foto da Sheyla redonda para perfil

--- js/conversas.js
+++ js/conversas.js
@@ -1,28 +1,29 @@
 document.addEventListener('DOMContentLoaded', function(){
     main = document.querySelector('main');
-    listamensagens = documento.querySelector('#mensagens')
-    var button = document.querySelector(".botao-enviar");
-    var input = document.querySelector("input");
+    listamensagens = document.querySelector('#mensagens')
+    button = document.querySelector(".botao-enviar");
+    console.log(button);
+    input = document.querySelector("input");
     button.addEventListener("click", function(event){
         var texto = input.value;
         li_novo = document.createElement("li");
-        div1 = li_novo.createElement('div');
+        div1 = document.createElement('div');
         h2_1 = document.createElement('h2');
         h2_1.innerHTML = 'Nome do Usuário';
         h3_1 = document.createElement('h3');
         h3_1.innerHTML = 'Horário';
         div1.appendChild(h2_1);
         div1.appendChild(h3_1);
-        section_novo.appendChild(div1);
+        li_novo.appendChild(div1);
         div2 = document.createElement('div');
         div2.classList.add("bolha-resposta");
         p1_2 = document.createElement("p");
         p1_2.innerHTML = texto;
         div2.appendChild(p1_2);
-        section_novo.appendChild(div2);
+        li_novo.appendChild(div2);
         span1 = document.createElement('span');
         span1.id = "notificacao-msg-4";
-        section_novo.appendChild(span1);
+        li_novo.appendChild(span1);
         listamensagens.appendChild(li_novo);
     });
 
